2c71fae1e6903ba49505efb3c7d61c8c5f14ef19
Rudy <lsofyan@binus.edu>
2021-10-21 01:52:46 +0700

    Update App.js

--- App.js
+++ App.js
@@ -3,6 +3,13 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './redux/store';
 import RootNavigator from './navigation/RootNavigator';
+import { LogBox } from 'react-native';
+
+// Ignore log notification by message
+LogBox.ignoreLogs(['Warning: ...']);
+
+//Ignore all log notifications
+LogBox.ignoreAllLogs();
 
 export default function App() {
   return (
